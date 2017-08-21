ac23aa053034bf9ef94d170931a1def633445b40
Anit Gandhi <anitgandhi@gmail.com>
2017-08-21 12:42:30 -0400

    add doc.go, fixes #6

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Package fpe implements the NIST recommended Format Preserving Encryption (FPE) FF1 and FF3 algorithms.
+
+NIST Recommendation SP 800-38G: http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-38G.pdf
+
+This package itself has nothing, the ff1 and ff3 sub-packages contain the API.
+
+*/
+package fpe
